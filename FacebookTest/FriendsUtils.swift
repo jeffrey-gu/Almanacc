3caf671e0222b52dc66f32b6dfc61e5fe30471d8
Jonathan Shieh <jonathanshieh@jesses-air-2.dhcp.wustl.edu>
2017-04-09 21:37:43 -0500

    Added a FriendsUtils.swift file - functions that handle graph requests

---
+++ FacebookTest/FriendsUtils.swift
@@ -0,0 +1,24 @@
+//
+//  FriendsUtils.swift
+//  
+//
+//  Created by Jonathan Shieh on 4/9/17.
+//
+//
+
+import Foundation
+
+func findFriends(){
+    let params = ["fields": "id, first_name, last_name, middle_name, name, email, picture"]
+    let graphRequest = GraphRequest(graphPath: "me/taggable_friends", parameters: params)
+    graphRequest.start { (urlResponse, requestResult) in
+        switch requestResult {
+        case .failed(let error):
+            print(error)
+        case .success(let graphResponse):
+            if let responseDictionary = graphResponse.dictionaryValue {
+                print(responseDictionary)
+            }
+        }
+    }
+}
